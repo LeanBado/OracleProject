6308ef10d81aef2f629f2d3a70b6a385e4ba15bd
LeanBado <leandro.bado.42@gmail.com>
2022-04-15 18:38:48 -0300

    modelo de prueba página de paciente y cálculo de IMC

--- proyectoIMC/js/principal.js
+++ proyectoIMC/js/principal.js
@@ -1,3 +1,35 @@
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+botonAdicionar.addEventListener("click", function(evento){
+    evento.preventDefault();
+
+    var form = document.querySelector("#form-boton");
+    var tBody = document.querySelector("#tabla-pacientes");
+    
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value
+
+    var pacienteTr = document.createElement("tr");
+    var nombreTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nombreTd.textContent = nombre;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    tBody.appendChild(pacienteTr);
+})
+
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var contador = 0; contador <pacientes.length; contador++){
